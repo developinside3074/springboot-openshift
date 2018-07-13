70fd32a3f4ed54aa6fa5770310b1f5679a8b5ecc
Osmar Gonzalez <developinside3074@gmail.com>
2018-07-13 09:31:03 -0500

    Primera subida

---
+++ src/main/java/pe/attlaris/openshift/OpenshiftApplication.java
@@ -0,0 +1,12 @@
+package pe.attlaris.openshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenshiftApplication.class, args);
+    }
+}
---
+++ src/main/java/pe/attlaris/openshift/controller/OpenShiftController.java
@@ -0,0 +1,19 @@
+package pe.attlaris.openshift.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/openshift")
+public class OpenShiftController {
+
+    @GetMapping(value = "/test")
+    public String getMessage(){
+
+        return "Esta en una prueba de un Proyecto SpringBoot en Openshift";
+    }
+
+
+}
